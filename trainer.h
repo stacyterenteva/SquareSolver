0d56cc3991c5f8b151cb16140b1258f09268c2b9
stacyterenteva <stacy.terenteva@gmail.com>
2026-08-23 21:43:41 +0300

    Полная бета версия тренера

--- trainer.h
+++ trainer.h
@@ -1,13 +1,13 @@
 #ifndef TRAINER_H
 #define TRAINER_H
 
-const int AMT_QUESTIONS = 3;
+const int AMT_QUESTIONS = 5;
 
-struct Test {char* question; double answer;};
+struct Test {const char* question; double answer;};
 
 void trainer_work();
 void trainer_asks(int question_number, int training_report[], Test* tests);
 void print_level(int amt_correct_answers);
 int count_correct_answers(int* training_report);
 
-#endif
+#endif /* TRAINER_H */
